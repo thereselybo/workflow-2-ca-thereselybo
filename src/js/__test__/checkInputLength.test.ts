9f4dc959d1de0eddcc43e7ded64645291ee3eb4b
Therese Lybo <therese.lybo@gmail.com>
2021-02-20 17:25:02 +0100

    making a failing test

--- src/js/__test__/checkInputLength.test.ts
+++ src/js/__test__/checkInputLength.test.ts
@@ -1,5 +1,5 @@
 import { checkInputLength } from "../utils/validation";
 
 test("check if input length after trimming is truthy", () => {
-  expect(checkInputLength("hello there")).toBeTruthy();
+  expect(checkInputLength("")).toBeTruthy();
 });
